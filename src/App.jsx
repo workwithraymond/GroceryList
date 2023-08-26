cbfa40798a3a8700ebe595d876860bc5b1cb888c
workwithraymond <workwithraymond01@gmail.com>
2023-08-26 17:21:45 -0400

    completed project

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import Form from './Form';
+import { nanoid } from 'nanoid';
+import Items from './items';
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+ if(list) {
+  list = JSON.parse(localStorage.getItem('list'));
+ }
+ else {
+  list = [];
+ }
+ return list;
+}
+
+const setLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items))
+}
+
+const App = () => {
+  getLocalStorage();
+  const [items, setItems] = useState(getLocalStorage());
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
+  };
+  const removeItem = (itemId) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+    setLocalStorage(newItems);
+  };
+
+  const editItem = (itemId) => {
+    const newItems = items.map((item) => {
+      if(item.id === itemId) {
+        const newItem = {...item, completed: !item.completed};
+        return newItem;
+      }
+      return item
+    });
+    setItems(newItems);
+    setLocalStorage(newItems);
+  }
+
+  return (
+  <section className='section-center'>
+    <Form addItem={addItem}/>
+    <Items items = {items} removeItem={removeItem} editItem= {editItem}/>
+    </section>);
+};
+
+export default App;
---
+++ src/items.jsx
@@ -0,0 +1,12 @@
+import SingleItem from "./SingleItem"
+
+const items = ({items, removeItem, editItem}) => {
+  return (
+    <div className="items">
+        {items.map((item) => {
+            return <SingleItem key={item.id} item= {item} removeItem = {removeItem} editItem= {editItem}/>;
+        })}
+    </div>
+  )
+}
+export default items
